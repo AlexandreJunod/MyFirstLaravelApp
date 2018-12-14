66aab83e6f9781209d64c4058127bf63c24ac639
JUNOD Alexandre <Alexandre.JUNOD@cpnv.ch>
2018-12-14 14:39:52 +0100

    Add ColorSeeder

--- database/seeds/ColorSeeder.php
+++ database/seeds/ColorSeeder.php
@@ -11,6 +11,15 @@ class ColorSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('colors')->insert(['name' => 'Rose']);
+        DB::table('colors')->insert(['name' => 'Noir']);
+        DB::table('colors')->insert(['name' => 'Jaune']);
+        DB::table('colors')->insert(['name' => 'Bleu']);
+        DB::table('colors')->insert(['name' => 'Orange']);
+        DB::table('colors')->insert(['name' => 'Rouge']);
+        DB::table('colors')->insert(['name' => 'Vert']);
+        DB::table('colors')->insert(['name' => 'Violet']);
+        DB::table('colors')->insert(['name' => 'Brun']);
+        echo "Add colors successful";
     }
 }
